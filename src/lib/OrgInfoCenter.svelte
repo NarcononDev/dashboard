e6c0fd24b861513e68ec4140ae878d2a7c12efb2
NarcononDev <roger@able.org>
2025-04-02 18:34:07 -0700

    Small updates

--- src/lib/OrgInfoCenter.svelte
+++ src/lib/OrgInfoCenter.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { currentUser, pb } from './pocketbase';
-  import {Chart} from 'chart.js'
+  import Chart from 'chart.js/auto';
 
   let organizations = $state([]);
   let selectedOrganization = $state({});
   let statistics = $state([]);
   let selectedStatistic = $state('');
   let currentView = $state('organizations')
+  
+  let statGraphData = [];
+  let statGraphLabels = [];
+  let statGraph;
+  
   async function changeView(isCurrentView, newView, data) {
     if(isCurrentView == 'organizations') {
       selectedOrganization = data;
@@ -19,6 +24,41 @@
       statistics = resultList.items;
     }
     currentView = newView;
+  }
+  async function loadStat(statId, statName) {
+    currentView = 'statdisplay'
+    const statFigList = await pb.collection('Statistic_Figures').getList(1, 50, {
+      filter: `statistic.id="${statId}" && organization.id="${selectedOrganization.id}"`, 
+      expand: 'week_ending_date'
+    });
+    statFigList.items.sort((a, b) => {
+      if (a.expand.week_ending_date.date > b.expand.week_ending_date.date) return 1;
+      if (a.expand.week_ending_date.date < b.expand.week_ending_date.date) return -1;
+      return 0;
+    })
+    for(let i = 0; i < statFigList.items.length; i++) {
+      statGraphData.push(statFigList.items[i].value)
+      statGraphLabels.push(statFigList.items[i].expand.week_ending_date.date)
+    }
+    const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+    let graphData = {
+      labels: statGraphLabels,
+      datasets: [{
+        label: statName,
+        borderColor: 'rgb(0, 0, 0)',
+        segment: {
+          borderColor: ctx => down(ctx, 'rgb(255,0,0)'),
+        },
+        data: statGraphData,
+        fill: false
+      }]
+    };    
+    let graphConfig = {
+      type: 'line',
+      data: graphData,
+    };
+    const ctx = statGraph.getContext('2d');  
+    new Chart(ctx, graphConfig as any);
   }
   onMount(async () => {
     const organizationList = await pb.collection('Organizations').getList(1, 50, {
@@ -46,9 +86,16 @@
 {#if currentView == 'statpicker' }
 <div class="pl-2 pr-2">
   {#each statistics as stat}
-    <button class="p-1 m-1 pointer">
+    <button 
+      onclick={() => loadStat(stat.id, stat.name)}
+      class="p-1 m-1 pointer">
       {stat.name}
     </button>
   {/each}
 </div>
+{/if}
+{#if currentView == 'statdisplay' }
+  <div>
+    <canvas bind:this={statGraph}></canvas>
+  </div>
 {/if}
